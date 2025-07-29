9e33838022b71ec3b181c67a911b09f36b3fe60a
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-07-29 17:25:03 +0300

    add pagination class to settings

--- dbsection/dbsection/settings.py
+++ dbsection/dbsection/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    "rest_framework",
+
     "app.users",
     "app.videos",
 ]
@@ -110,6 +112,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
